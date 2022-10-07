260b07be684e302c748ae39001a18755f296b097
uddhavvyas <uddhav.vyas@vsdtechno.com>
2022-10-07 19:53:08 +0530

    Added changes: Input Validations

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 	"sap/ui/Device",
 	"sap/m/WizardProgressNavigator",
 	"sap/m/WizardRenderer"
-], function(Controller, UploadCollectionParameter,Device) {
+], function(Controller, UploadCollectionParameter, Device) {
 	"use strict";
 
 	return Controller.extend("safetysuitezclaimemployee.controller.Main", {
@@ -30,7 +30,7 @@ sap.ui.define([
 		onCreateIncidentPress: function(oEvent) {
 			var urlString = document.location.href.split("/");
 			var host = urlString[2];
-			window.open("https://"+host+"/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#CNet-MyIncidents", "_blank");
+			window.open("https://" + host + "/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#CNet-MyIncidents", "_blank");
 			//window.open("https://sapsdev.c-net.com.au/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#CNet-MyIncidents", "_blank");
 		}, //To open create incident app in new window.
 
@@ -54,59 +54,59 @@ sap.ui.define([
 			}
 			this.WizardTitle = "StartClaim";
 			this.claimWizardDialog.open();
-			sap.ui.getCore().byId("claimFormWizard")._getProgressNavigator().ontap = function(){};
-			sap.ui.getCore().byId("claimFormWizard")._scrollHandler = function(){
+			sap.ui.getCore().byId("claimFormWizard")._getProgressNavigator().ontap = function() {};
+			sap.ui.getCore().byId("claimFormWizard")._scrollHandler = function() {
 				if (this._scrollLocked) {
-				return;
-			}
-			if(Device.browser === undefined){
-					var scrollTop =  document.documentElement.querySelector(".sapMWizardStepContainer").scrollTop; 
-			}
-			else{
-				var scrollTop =  event.target.scrollTop;
-			}
-		
-				var progressNavigator = this._getProgressNavigator(),
-				currentStepDOM = this._stepPath[progressNavigator.getCurrentStep() - 1].getDomRef();
-
-			if (!currentStepDOM) {
-				return;
-			}else{
-				var wizardStep = currentStepDOM.dataset.sapUi;
-				if (wizardStep === "attachmentStep") {
-					sap.ui.getCore().byId("claimWizardNextBtn").setVisible(false);
-				} else if (wizardStep === "personalDetailStep") {
-					sap.ui.getCore().byId("claimWizardPrevBtn").setVisible(false);
-				} else {
-					sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
-					sap.ui.getCore().byId("claimWizardPrevBtn").setVisible(true);
+					return;
 				}
-			}
+				if (Device.browser === undefined) {
+					var scrollTop = document.documentElement.querySelector(".sapMWizardStepContainer").scrollTop;
+				} else {
+					var scrollTop = event.target.scrollTop;
+				}
 
-			var stepHeight = currentStepDOM.clientHeight,
-				stepOffset = currentStepDOM.offsetTop,
-				stepChangeThreshold = 100;
-
-			if (scrollTop + stepChangeThreshold >= stepOffset + stepHeight && progressNavigator._isActiveStep(progressNavigator._currentStep + 1)) {
-				progressNavigator.nextStep();
-			}
-
-			var aSteps = this.getSteps();
-			// change the navigator current step
-			for (var index = 0; index < aSteps.length; index++) {
-				if (scrollTop + stepChangeThreshold <= stepOffset) {
-					progressNavigator.previousStep();
-
-					// update the currentStep reference
+				var progressNavigator = this._getProgressNavigator(),
 					currentStepDOM = this._stepPath[progressNavigator.getCurrentStep() - 1].getDomRef();
 
-					if (!currentStepDOM) {
-						break;
+				if (!currentStepDOM) {
+					return;
+				} else {
+					var wizardStep = currentStepDOM.dataset.sapUi;
+					if (wizardStep === "attachmentStep") {
+						sap.ui.getCore().byId("claimWizardNextBtn").setVisible(false);
+					} else if (wizardStep === "personalDetailStep") {
+						sap.ui.getCore().byId("claimWizardPrevBtn").setVisible(false);
+					} else {
+						sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
+						sap.ui.getCore().byId("claimWizardPrevBtn").setVisible(true);
 					}
-
-					stepOffset = currentStepDOM.offsetTop;
 				}
-			}
+
+				var stepHeight = currentStepDOM.clientHeight,
+					stepOffset = currentStepDOM.offsetTop,
+					stepChangeThreshold = 100;
+
+				if (scrollTop + stepChangeThreshold >= stepOffset + stepHeight && progressNavigator._isActiveStep(progressNavigator._currentStep +
+						1)) {
+					progressNavigator.nextStep();
+				}
+
+				var aSteps = this.getSteps();
+				// change the navigator current step
+				for (var index = 0; index < aSteps.length; index++) {
+					if (scrollTop + stepChangeThreshold <= stepOffset) {
+						progressNavigator.previousStep();
+
+						// update the currentStep reference
+						currentStepDOM = this._stepPath[progressNavigator.getCurrentStep() - 1].getDomRef();
+
+						if (!currentStepDOM) {
+							break;
+						}
+
+						stepOffset = currentStepDOM.offsetTop;
+					}
+				}
 			};
 
 			sap.ui.getCore().byId("html").setContent("<canvas id='signature-pad' width='200px' height='200px' class='signature-pad'></canvas>");
@@ -119,10 +119,10 @@ sap.ui.define([
 			sap.ui.getCore().byId("injuryTabStartBtn").setEnabled(false);
 			sap.ui.getCore().byId("injuryTabCreateIncBtn").setEnabled(false);
 
-			if(this.ConfidentialColumnText === "Yes"){
-				if(!this.confidentialPopup){
-				this.confidentialPopup = sap.ui.xmlfragment("safetysuitezclaimemployee.fragment.confidentialPopup", this);
-				this.getView().addDependent(this.confidentialPopup);
+			if (this.ConfidentialColumnText === "Yes") {
+				if (!this.confidentialPopup) {
+					this.confidentialPopup = sap.ui.xmlfragment("safetysuitezclaimemployee.fragment.confidentialPopup", this);
+					this.getView().addDependent(this.confidentialPopup);
 				}
 				this.confidentialPopup.open();
 			}
@@ -133,31 +133,106 @@ sap.ui.define([
 			this._iSelectedStepIndex = this._oWizard.getCurrentStep();
 			var oNextStep = this._oWizard.getSteps()[this._iSelectedStepIndex + 1];
 
-			if (this._oSelectedStep && !this._oSelectedStep.bLast) {
-				this._oWizard.goToStep(oNextStep, true);
+			if (this._oWizard.getCurrentStep() === "personalDetailStep") {
+				var titleInput = sap.ui.getCore().byId("titleInput");
+				var familyNameInput = sap.ui.getCore().byId("familyNameInput");
+				var givenNameInput = sap.ui.getCore().byId("givenNameInput");
 
+				if (titleInput.getValue() === "" || titleInput.getValue() === undefined) {
+					titleInput.setValueState("Error");
+				} else if (familyNameInput.getValue() === "" || familyNameInput.getValue() === undefined) {
+					familyNameInput.setValueState("Error");
+				} else if (givenNameInput.getValue() === "" || givenNameInput.getValue() === undefined) {
+					givenNameInput.setValueState("Error");
+				} else {
+					if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+						this._oWizard.goToStep(oNextStep, true);
+
+					} else {
+						this._oWizard.nextStep();
+					}
+				}
+
+			} else if (this._oWizard.getCurrentStep() === "injuryDetailStep") {
+				var injuryDatePicker = sap.ui.getCore().byId("injuryDatePicker");
+				var stoppedWorkDatePicker = sap.ui.getCore().byId("stoppedWorkDatePicker");
+				if (injuryDatePicker.getValue() === "" || injuryDatePicker.getValue() === undefined) {
+					injuryDatePicker.setValueState("Error");
+				} else if (stoppedWorkDatePicker.getValue() === "" || stoppedWorkDatePicker.getValue() === undefined) {
+					stoppedWorkDatePicker.setValueState("Error");
+				} else {
+					if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+						this._oWizard.goToStep(oNextStep, true);
+
+					} else {
+						this._oWizard.nextStep();
+					}
+				}
+			} else if (this._oWizard.getCurrentStep() === "employmentDetailStep") {
+				var appliesToYouDropdown = sap.ui.getCore().byId("appliesToYouDropdown");
+				if (appliesToYouDropdown.getValue() === "" || appliesToYouDropdown.getValue() === undefined) {
+					appliesToYouDropdown.setValueState("Error");
+				} else {
+					if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+						this._oWizard.goToStep(oNextStep, true);
+
+					} else {
+						this._oWizard.nextStep();
+					}
+				}
+			} else if (this._oWizard.getCurrentStep() === "workerEarningStep") {
+				var weeklyShiftInput = sap.ui.getCore().byId("weeklyShiftInput");
+				var weeklyOvertimeInput = sap.ui.getCore().byId("weeklyOvertimeInput");
+				if (weeklyShiftInput.getValue() === "" || weeklyShiftInput.getValue() === undefined) {
+					weeklyShiftInput.setValueState("Error");
+				} else if (weeklyOvertimeInput.getValue() === "" || weeklyOvertimeInput.getValue() === undefined) {
+					weeklyOvertimeInput.setValueState("Error");
+				} else {
+					if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+						this._oWizard.goToStep(oNextStep, true);
+
+					} else {
+						this._oWizard.nextStep();
+					}
+				}
+			} else if (this._oWizard.getCurrentStep() === "workerDecStep") {
+				var canvas = document.getElementById("signature-pad");
+				// roughString variable is used for storing the string of blank signature box and its used below for validation.
+				var roughString =
+					"ZGF0YTppbWFnZS9qcGVnO2Jhc2U2NCwvOWovNEFBUVNrWkpSZ0FCQVFBQUFRQUJBQUQvNGdJb1NVTkRYMUJTVDBaSlRFVUFBUUVBQUFJWUFBQUFBQVF3QUFCdGJuUnlVa2RDSUZoWldpQUFBQUFBQUFBQUFBQUFBQUJoWTNOd0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFRQUE5dFlBQVFBQUFBRFRMUUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBbGtaWE5qQUFBQThBQUFBSFJ5V0ZsYUFBQUJaQUFBQUJSbldGbGFBQUFCZUFBQUFCUmlXRmxhQUFBQmpBQUFBQlJ5VkZKREFBQUJvQUFBQUNoblZGSkRBQUFCb0FBQUFDaGlWRkpEQUFBQm9BQUFBQ2gzZEhCMEFBQUJ5QUFBQUJSamNISjBBQUFCM0FBQUFEeHRiSFZqQUFBQUFBQUFBQUVBQUFBTVpXNVZVd0FBQUZnQUFBQWNBSE1BVWdCSEFFSUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFGaFpXaUFBQUFBQUFBQnZvZ0FBT1BVQUFBT1FXRmxhSUFBQUFBQUFBR0taQUFDM2hRQUFHTnBZV1ZvZ0FBQUFBQUFBSktBQUFBK0VBQUMyejNCaGNtRUFBQUFBQUFRQUFBQUNabVlBQVBLbkFBQU5XUUFBRTlBQUFBcGJBQUFBQUFBQUFBQllXVm9nQUFBQUFBQUE5dFlBQVFBQUFBRFRMVzFzZFdNQUFBQUFBQUFBQVFBQUFBeGxibFZUQUFBQUlBQUFBQndBUndCdkFHOEFad0JzQUdVQUlBQkpBRzRBWXdBdUFDQUFNZ0F3QURFQU52L2JBRU1BQXdJQ0FnSUNBd0lDQWdNREF3TUVCZ1FFQkFRRUNBWUdCUVlKQ0FvS0NRZ0pDUW9NRHd3S0N3NExDUWtORVEwT0R4QVFFUkFLREJJVEVoQVREeEFRRVAvYkFFTUJBd01EQkFNRUNBUUVDQkFMQ1FzUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFUC9BQUJFSUFNZ0F5QU1CSWdBQ0VRRURFUUgveEFBVkFBRUJBQUFBQUFBQUFBQUFBQUFBQUFBQUNmL0VBQlFRQVFBQUFBQUFBQUFBQUFBQUFBQUFBQUQveEFBVUFRRUFBQUFBQUFBQUFBQUFBQUFBQUFBQS84UUFGQkVCQUFBQUFBQUFBQUFBQUFBQUFBQUFBUC9hQUF3REFRQUNFUU1SQUQ4QWxVQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUQvOWs9";
+				this.signString = btoa(encodeURI(canvas.toDataURL('image/jpeg').replace("data:image/jpeg:base64,", "")));
+				var declarationDatePicker = sap.ui.getCore().byId("declarationDatePicker");
+				if (declarationDatePicker.getValue() === "" || declarationDatePicker.getValue() === undefined) {
+					declarationDatePicker.setValueState("Error");
+				} else if (roughString === this.signString) {
+					canvas.style.borderColor = "red";
+				} else {
+					console.log(this.signString);
+					canvas.style.borderColor = "black";
+					if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+						this._oWizard.goToStep(oNextStep, true);
+
+					} else {
+						this._oWizard.nextStep();
+					}
+
+				}
 			} else {
-				this._oWizard.nextStep();
+				if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+					this._oWizard.goToStep(oNextStep, true);
+
+				} else {
+					this._oWizard.nextStep();
+				}
 			}
 			this._iSelectedStepIndex++;
 			this._oSelectedStep = oNextStep;
 
 			if (this._oWizard.getCurrentStep() === "attachmentStep") {
-				var canvas = document.getElementById("signature-pad");
-				// roughString variable is used for storing the string of blank signature box and its used below for validation.
-				var roughString = "ZGF0YTppbWFnZS9qcGVnO2Jhc2U2NCwvOWovNEFBUVNrWkpSZ0FCQVFBQUFRQUJBQUQvNGdJb1NVTkRYMUJTVDBaSlRFVUFBUUVBQUFJWUFBQUFBQVF3QUFCdGJuUnlVa2RDSUZoWldpQUFBQUFBQUFBQUFBQUFBQUJoWTNOd0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFRQUE5dFlBQVFBQUFBRFRMUUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBbGtaWE5qQUFBQThBQUFBSFJ5V0ZsYUFBQUJaQUFBQUJSbldGbGFBQUFCZUFBQUFCUmlXRmxhQUFBQmpBQUFBQlJ5VkZKREFBQUJvQUFBQUNoblZGSkRBQUFCb0FBQUFDaGlWRkpEQUFBQm9BQUFBQ2gzZEhCMEFBQUJ5QUFBQUJSamNISjBBQUFCM0FBQUFEeHRiSFZqQUFBQUFBQUFBQUVBQUFBTVpXNVZVd0FBQUZnQUFBQWNBSE1BVWdCSEFFSUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFGaFpXaUFBQUFBQUFBQnZvZ0FBT1BVQUFBT1FXRmxhSUFBQUFBQUFBR0taQUFDM2hRQUFHTnBZV1ZvZ0FBQUFBQUFBSktBQUFBK0VBQUMyejNCaGNtRUFBQUFBQUFRQUFBQUNabVlBQVBLbkFBQU5XUUFBRTlBQUFBcGJBQUFBQUFBQUFBQllXVm9nQUFBQUFBQUE5dFlBQVFBQUFBRFRMVzFzZFdNQUFBQUFBQUFBQVFBQUFBeGxibFZUQUFBQUlBQUFBQndBUndCdkFHOEFad0JzQUdVQUlBQkpBRzRBWXdBdUFDQUFNZ0F3QURFQU52L2JBRU1BQXdJQ0FnSUNBd0lDQWdNREF3TUVCZ1FFQkFRRUNBWUdCUVlKQ0FvS0NRZ0pDUW9NRHd3S0N3NExDUWtORVEwT0R4QVFFUkFLREJJVEVoQVREeEFRRVAvYkFFTUJBd01EQkFNRUNBUUVDQkFMQ1FzUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFQkFRRUJBUUVCQVFFUC9BQUJFSUFNZ0F5QU1CSWdBQ0VRRURFUUgveEFBVkFBRUJBQUFBQUFBQUFBQUFBQUFBQUFBQUNmL0VBQlFRQVFBQUFBQUFBQUFBQUFBQUFBQUFBQUQveEFBVUFRRUFBQUFBQUFBQUFBQUFBQUFBQUFBQS84UUFGQkVCQUFBQUFBQUFBQUFBQUFBQUFBQUFBUC9hQUF3REFRQUNFUU1SQUQ4QWxVQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUQvOWs9";
-				this.signString = btoa(encodeURI(canvas.toDataURL('image/jpeg').replace("data:image/jpeg:base64,", "")));
-				if (roughString === this.signString) {
-					sap.m.MessageBox.information("Please fill the box with signature");
-					this._oWizard.previousStep();
-					sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
-						}
-				else {
-					console.log(this.signString);
-					sap.ui.getCore().byId("claimWizardNextBtn").setVisible(false);
-					sap.ui.getCore().byId("claimSubmitBtn").setEnabled(true);
-				}
-				
+
+				sap.ui.getCore().byId("claimWizardNextBtn").setVisible(false);
+				sap.ui.getCore().byId("claimSubmitBtn").setEnabled(true);
+
 			} else if (this._oWizard.getCurrentStep() === "personalDetailStep") {
 				sap.ui.getCore().byId("claimWizardPrevBtn").setVisible(false);
 			} else {
@@ -189,10 +264,9 @@ sap.ui.define([
 		}, // Code for previous button in the main claim wizard control.
 
 		handleWizardCancel: function(oEvent) {
-			if(oEvent.getSource().getParent().getId() === "confidentialPopupDialog"){
+			if (oEvent.getSource().getParent().getId() === "confidentialPopupDialog") {
 				oEvent.getSource().getParent().close();
-			}
-			else if (this.WizardTitle === "StartClaim") {
+			} else if (this.WizardTitle === "StartClaim") {
 				this.claimWizardDialog.close();
 				sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
 				sap.ui.getCore().byId("claimSubmitBtn").setEnabled(false);
@@ -302,16 +376,16 @@ sap.ui.define([
 						press: function() {
 							this.oApproveDialog.close();
 							var sSource = sap.ui.require.toUrl("safetysuitezclaimemployee/Attachment_Sample_Files/2056106_E_20220914.pdf");
-								this.claimWizardDialog.close();
-								sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
-								sap.ui.getCore().byId("claimSubmitBtn").setEnabled(false);
-								this._oWizard.setCurrentStep("personalDetailStep");
-								this._pdfViewer = new sap.m.PDFViewer();
-								this.getView().addDependent(this._pdfViewer);
-								this._pdfViewer.setSource(sSource);
-								this._pdfViewer.setTitle("Details of Claim Form");
-								this._pdfViewer.open();
-							
+							this.claimWizardDialog.close();
+							sap.ui.getCore().byId("claimWizardNextBtn").setVisible(true);
+							sap.ui.getCore().byId("claimSubmitBtn").setEnabled(false);
+							this._oWizard.setCurrentStep("personalDetailStep");
+							this._pdfViewer = new sap.m.PDFViewer();
+							this.getView().addDependent(this._pdfViewer);
+							this._pdfViewer.setSource(sSource);
+							this._pdfViewer.setTitle("Details of Claim Form");
+							this._pdfViewer.open();
+
 						}.bind(this)
 					}),
 					endButton: new sap.m.Button({
@@ -457,14 +531,22 @@ sap.ui.define([
 				  penWidth : '1'
 			})*/
 		},
-		
-		onOpenHelpPopup: function(oEvent){
-			if(!this.helpPopup){
+
+		onOpenHelpPopup: function(oEvent) {
+			if (!this.helpPopup) {
 				this.helpPopup = sap.ui.xmlfragment("safetysuitezclaimemployee.fragment.HelpPopup", this);
 				this.getView().addDependent(this.helpPopup);
 			}
 			this.helpPopup.openBy(oEvent.getSource());
+		},
+
+		checkInputValidation: function(oEvent) {
+			if (oEvent.getParameters().value !== "" || oEvent.getParameters().value !== undefined) {
+				oEvent.getSource().setValueState("None");
+			} else {
+				oEvent.getSource().setValueState("Error");
+			}
 		}
-		
+
 	});
 });
